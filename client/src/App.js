e8cc3cecf26a7398d6f2b682aebf9d63fc9d61e9
MakeNoise01 <marianomotos50@gmail.com>
2020-10-05 19:19:10 -0300

    CRUD user en proceso

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import NewClient from './components/CRUD/client';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+       {/*  <Route 
+        exact 
+        path='/clientRegister' component={NewClient} />
+        <Link to="/clientRegister">
+					<button> JOSESITO</button>
+				</Link> */}
+       
+       <NewClient></NewClient>
     </div>
   );
 }
 
-export default App;
+export default connect(null, null)(App);
