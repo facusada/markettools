99095b3f4bbf011ed621dade1cbdc3d3904f7b28
unknown <facundosadava1@gmail.com>
2020-10-09 15:35:47 -0300

    Nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
-import {
-   Collapse,
-   Navbar, 
-   NavbarToggler, 
-   NavbarBrand, 
-   Nav, 
-   NavItem, 
-   NavLink 
-  } from 'reactstrap';
+import React from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Header = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+function Header() {
+  const classes = useStyles();
 
   return (
-    <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">MarketTools</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            MarketTools
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
 
-export default Header;
+// function mapStateToProps(state) {
+//   return {
+
+//   }
+// }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+
+//   }
+// }
+
+export default connect(null, null)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
