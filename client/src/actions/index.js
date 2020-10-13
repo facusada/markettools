c2e8a6b890dcfcd09378fd9ca0ddab317dafb1ca
unknown <facundosadava1@gmail.com>
2020-10-13 18:19:42 -0300

    Algunas actions necesarias

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-export const PRUEBA_API = "PRUEBA_API";  
+export const PRUEBA_API = "PRUEBA_API";
+export const ADD_TOOL = "ADD_TOOL";
+export const ADD_CLIENT = "ADD_CLIENT";
+export const CARGA_DB = "CARGA_DB";
+export const GET_CLIENT = "GET_CLIENT";
+export const GET_TOOL = "GET_TOOL";
+export const GET_ALL_TOOLS = "GET_ ALL_TOOLS"
 
 
-export function infoMovie (apiKey, ciudad ) {          
-    return function(dispatch) {        
+export function infoMovie (apiKey, ciudad ) {
+    return function(dispatch) {
       return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`)
         .then(result => result.data)
         .then(data => {
@@ -15,19 +21,74 @@ export function infoMovie (apiKey, ciudad ) {
           console.log("El Actions ",data)
         })
     };
-     
   }
 
-export function cargardb () {          
-  return function(dispatch) {        
+export function cargardb () {
+  return function(dispatch) {
     return axios.post(`http://localhost:3005/registerhd`)
       .then(result => result.data)
       .then(data => {
-        dispatch({          
+        dispatch({
+          type: "CARGA_DB",
           payload: data
         })
-        console.log("la carga DB ",data)
+        console.log("la carga DB ", data)
       })
   };
-    
+}
+
+export function getClient() {
+  return function(dispatch) {
+    return axios.get(`http://localhost:3005/client`)
+    .then(result => result.data)
+    .then(data => {
+      dispatch({
+        type: "GET_CLIENT",
+        payload: data
+      })
+      console.log("Estos son todos los clientes", data)
+    })
+  }
+}
+
+export function getAllTools() {
+  return function(dispatch) {
+    return axios.get(`http://localhost:3005/tools`)
+    .then(result => result.data)
+    .then(data => {
+      dispatch({
+        type: "GET_ALL_TOOLS",
+        payload: data
+      })
+      console.log("Todas las tools", data)
+    })
+  }
+}
+
+export function getTool() {
+  return function(dispatch) {
+    return axios.get(`http://localhost:3005/tools/:id`)
+    .then(result => result.data)
+    .then(data => {
+      dispatch({
+        type: "GET_TOOL",
+        payload: data
+      })
+      console.log("La tool es", data)
+    })
+  }
+}
+
+export function addTool(payload) {
+  return {
+    type: "ADD_TOOL",
+    payload
+  }
+}
+
+export function addClient(payload) {
+  return {
+    type: "ADD_CLIENT",
+    payload
+  }
 }
